e48c10fa8c3de965d4a568630b61bbbdfedf522e
willydelerue <willy.delerue@gmail.com>
2023-11-01 14:41:52 +0100

    transformer le texte en HTML

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React, { useState } from 'react';
+import Marked from 'marked';
 import './App.css';
 
 function App() {
 
   // State
-  const[texte, setTexte] = useState('Ma zone texte par défaut');
+  const [texte, setTexte] = useState('# `Prévisualisateur - Believemy`\n### Oh ! De la magie !\nEn réalité, pas vraiment : ceci s\'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !\n\n### Cas d\'utilisation\n* *italique*\n* **gras**\n* `monospace`\n* ~~barré~~\n* # h1\n* ## h2\n* ### h3\n* #### etc\n[Believemy](https://believemy.com)');
 
-  //Fonctions
-  const changeTextHandler = (event) => {
-    setTexte(event.target.value)
+  // Fonctions
+  const changeTextHandler = event => {
+    setTexte(event.target.value);
   };
+
+  const markdownToHTML = () => {
+    return {__html: Marked(texte)};
+  }
 
   return (
     <div className="App">
-      <div className='elements'>
+      <div className="elements">
 
-        <div className='element'>
-          <textarea 
-          value= {texte}
-          onChange={(e) => changeTextHandler(e)}>
+        <div className="element">
+          <textarea
+            rows="30"
+            value={texte}
+            onChange={(e) => changeTextHandler(e)}>
           </textarea>
         </div>
 
-        <div className='element'>
-          <code>Test</code>
+        <div className="element">
+          <div dangerouslySetInnerHTML={markdownToHTML()}></div>
         </div>
 
       </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import{ Marked } from 'marked';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
